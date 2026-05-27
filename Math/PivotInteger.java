ca4aa6d4d80e0c8e0b12a054a9ed917a8bb459fa
CapnHazard <modiyash301@gmail.com>
2026-05-27 12:17:01 +0530

    PivotInteger

---
+++ Math/PivotInteger.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PivotInteger {
+    public int pivotInt(int n) {
+        int x = 0; int sum1 = 0; int sum2 = 0;
+        for(int i = 1; i <= n; i++) {
+            sum1 = sum1 + i;
+            for(int j = i; j <= n; j++) {
+                sum2 = 0;
+                sum2 = sum2 + j;
+                if(sum1 == sum2) {
+                    x = i;
+                }
+            }
+        }
+        return x;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        PivotInteger obj = new PivotInteger();
+        System.out.println(obj.pivotInt(n));
+        sc.close();
+    }
+}
